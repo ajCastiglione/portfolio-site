83f6f78054dcc44f5ba481fb9636c15b532326bd
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-09-17 23:01:43 -0400

    added header content

---
+++ src/partials/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "./../logo.png";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header-top container">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="header-contact">
+            <Link to="/contact">Get In Touch!</Link>
+          </div>
+        </div>
+        <div className="header-content">
+          <h2 className="header-subtitle">
+            Frontend Developer, Entrepreneur &amp; Enthusiast
+          </h2>
+          <p className="header-subcontent">
+            Building stunning websites is not only my job,
+            <em> but my passion</em>
+          </p>
+        </div>
+      </header>
+    );
+  }
+}
