be737ca53a7d564a71f94fff35ec040ef933dc49
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-09-21 19:04:47 -0400

    added import for footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Header from "./partials/Header";
 import Body from "./partials/Body";
+import Footer from "./partials/Footer";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Body />
+        <Footer />
       </div>
     );
   }
